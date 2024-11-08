0064a8ae02815b5d6c6082caef0cc05637084398
WangYuhang <153092435+Terialion@users.noreply.github.com>
2024-11-08 05:39:23 +0000

    Finish the project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# pdf-convert-word
+# pdf-to-word
+Use python to convert pdf to word
+## Step:
+1. __Requirement:__  
+    1.1 Have installed the __os__ and __pdf2docx__.
+    ```python
+    import os
+    from pdf2docx import Converter
+    ```
+    1.2 Have the python ebvironment.  
+2. __How to use__  
+    ```python
+    input_folder = ''
+    output_folder = ''
+    ```
+    Fill in the folder where your pdf is located, and fill in the folder where you want to store your word.
+    Run the program, then you will find the word in the __output_folder__
--- code/pdftoword.py
+++ code/pdftoword.py
@@ -1,14 +1,20 @@
-import pdf2doc as Convert
 import os
+from pdf2docx import Converter
 
-dir_path = os.getcwd();
-for file in os.listdir(dir_path):
-    filetype = file.split('.')[1]
-    if filetype == "pdf":
-        pdf_name = file.split('.')[0]
-        word_name = pdf_name + ".docx"
-        cv = Convert(file)
-        cv.convert(word_name)
-        cv.close
-    else:
-        continue
+def folder_pdf_convert_word(inputfolder, outputfolder):
+    if not os.path.exists(outputfolder):
+        os.makedirs(outputfolder)
+
+    for file in os.listdir(inputfolder):
+        if file.endswith('.pdf'):
+            pdf_name = file.split('.')[0]
+            word_name = pdf_name + ".docx"
+            cv = Converter(file)
+            cv.convert(word_name)
+            cv.close()
+        else:
+            continue
+
+input_folder = ''
+output_folder = ''
+folder_pdf_convert_word(input_folder, output_folder)
